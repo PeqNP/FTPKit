79ae5a6a73ed1224c53a7f72f325bc986abd3fc4
Eric Chamberlain <eric.chamberlain@hotmail.com>
2014-03-09 11:36:03 -0700

    Moved compiled libs and headers in different folders. I’m hoping this removes the duplicate symbol issue.

--- FTPKit/FTPKit.h
+++ FTPKit/FTPKit.h
@@ -1,9 +1,9 @@
 #import "ftplib.h"
 
-#import <FTPKit/FTPClient.h>
-#import <FTPKit/FTPCredentials.h>
-#import <FTPKit/FTPHandle.h>
-#import <FTPKit/FTPRequest.h>
+#import "FTPClient.h"
+#import "FTPCredentials.h"
+#import "FTPHandle.h"
+#import "FTPRequest.h"
 
 #ifndef __CFNETWORK__
 #pragma message("CFNetwork framework not found in project, or not included in precompiled header. Be sure to include all required frameworks.")
