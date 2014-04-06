bba10878180fc675aee4cd38b6b2447f67e10a65
Eric Chamberlain <eric.chamberlain@hotmail.com>
2014-04-06 14:34:23 -0700

    No longer need CFNetwork.

--- FTPKit/FTPKit.h
+++ FTPKit/FTPKit.h
@@ -2,7 +2,3 @@
 #import "FTPCredentials.h"
 #import "FTPHandle.h"
 #import "FTPClient.h"
-
-#ifndef __CFNETWORK__
-#pragma message("CFNetwork framework not found in project, or not included in precompiled header. Be sure to include all required frameworks.")
-#endif
